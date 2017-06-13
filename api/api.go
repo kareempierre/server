613734895b7fb36199c39eda95fe2d295c610c04
kareem <kareempierre@ymail.com>
2017-06-13 14:55:11 -0600

    added security checks

--- api/api.go
+++ api/api.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"encoding/json"
 
 	"github.com/codegangsta/negroni"
@@ -21,7 +23,18 @@ import (
 // UserCredentials requires an Email and Password
 type UserCredentials struct {
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password []byte `json:"password"`
+}
+
+// UserConstruct is what you get back from the database
+type UserConstruct struct {
+	FirstName    string
+	LastName     string
+	Email        string
+	Password     []byte
+	Organization string
+	Admin        bool
+	Creator      bool
 }
 
 var (
@@ -42,6 +55,7 @@ func API() {
 
 	// Public endpoints
 	v2.HandleFunc("/auth", authHandler).Methods("POST")
+	v2.HandleFunc("/auth/create/user", createUser).Methods("POST")
 
 	// Protected endpoints
 	protectedUserBaseRoute := mux.NewRouter()
@@ -62,12 +76,22 @@ func API() {
 	http.ListenAndServe(":12000", myRouter)
 }
 
+func createUser(res http.ResponseWriter, req *http.Request) {
+	// This takes all the information passed through as json data
+	// and creates a user
+
+	// Hash the password and check to see if the email address is already in use
+
+	// save to postgres database
+	// return a token
+
+}
+
 // authHandler is used to authenticate the user logging in
 func authHandler(res http.ResponseWriter, req *http.Request) {
-	var user, loggedInUser UserCredentials
-	//var email, password string
-	// Set Content header type on response
-	//res.Header().Set("Content-Type", "application/json")
+	var user UserCredentials
+	var dbUser UserConstruct
+
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
 		res.WriteHeader(http.StatusForbidden)
@@ -75,13 +99,25 @@ func authHandler(res http.ResponseWriter, req *http.Request) {
 	}
 
 	// DB query needs to be made here
-	rowErr := DB.QueryRow(`SELECT email, password  FROM users WHERE email=$1`, user.Email).Scan(&loggedInUser.Email, &loggedInUser.Password)
+	row := DB.QueryRow(`SELECT firstname, lastname, email, organization, creator, admin, password FROM users WHERE email=$1`, user.Email)
+	rowErr := row.Scan(&dbUser.FirstName, &dbUser.LastName, &dbUser.Email, &dbUser.Organization,
+		&dbUser.Creator, &dbUser.Admin, &dbUser.Password)
+
 	if rowErr != nil || rowErr == sql.ErrNoRows {
 		log.Fatal("Error: Fetching Rows from db")
 		return
 	}
 
-	fmt.Println(loggedInUser)
+	err = bcrypt.CompareHashAndPassword(dbUser.Password, user.Password)
+	if err != nil {
+		res.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(res, "Invalid password")
+	}
+
+	//res.Header().Set("Content-Type", "application/json")
+	// create token and send it back as json data using json.unmarshal
+	// will have to read up on this
+	fmt.Println(dbUser)
 }
 
 // usersHandler returns all users for a specific organization
